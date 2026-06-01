53621c07eb418a23f3d3b636d906e23f84803b2b
gabriellycama <gabrielly.camargo.pereira@escola.pr.gov.br>
2026-06-01 11:32:23 -0300

    Add files via upload

--- lucas_teste_ 13=04/conta.py
+++ lucas_teste_ 13=04/conta.py
@@ -3,7 +3,8 @@ class Conta:
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
-        self.__limite = limite
+        self.__limite_especial = limite
+        self.__codigo_banco = '001'
 
     def extrato(self):
         print(f"Saldo: {self.__saldo} do titular {self.__titular}")
@@ -14,14 +15,22 @@ class Conta:
         else:
             self.__saldo += valor
 
+    def __saque_permitido(self,valor_saque):
+        valor_disponivel_saque = self.__saldo + self.__limite_especial 
+        return valor_saque <= valor_disponivel_saque
+
     def sacar(self, valor):
-        if(self.__saldo < valor):
+        if (self.__saque_permitido(valor)):
             print("Saldo insuficiente")
-        else:
             self.__saldo -= valor
+        else:
+            print(f"o valor {valor} passou do limite")
+
+
+    
 
     def transferir(self, valor, destino):
-        if(self.__saldo < valor) or (valor < 0):
+        if(self.__saldo < valor) or (valor <= 0):
             print("Não é possível realizar a tranferência")
         else:
             self.sacar(valor)
@@ -38,18 +47,26 @@ class Conta:
     @property
     
     def get_limite(self):
-        return self.__limite
+        return self.__limite_especial
     
     @property
     def get_numero(self):
         return self.__numero
     
 
+    @property
+    def get_numero(self):
+        return self.__codigo_banco
+
     #metudos para manipular os valores da  
+    @staticmethod
+    def codigo_banco():
+        return '001'
     
-    @limite.setter
     def set_limite(self,limite):
-        self.__limite = limite
+        self.__limite_especial = limite
 
-
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001','caixa':'104','bradesco':'237'}
     
--- lucas_teste_ 13=04/exec.py
+++ lucas_teste_ 13=04/exec.py
@@ -3,6 +3,8 @@ from conta import Conta
 conta1 = Conta(535, "Ricardo", 550.0, 5000.0)
 conta2 = Conta(555, "Luana", 100.0, 1000.0)
 
+print(conta1.codigo_banco())
+
 #conta1.extrato()
 #conta1.sacar(5)
 #conta1.extrato()
@@ -22,6 +24,6 @@ conta2 = Conta(555, "Luana", 100.0, 1000.0)
 #conta1.set_limite(200)
 #print(conta1.get_limite())
 
-conta1.__limite =2000
-print(conta1)
+#onta1.__limite =20
+#print(conta2)
 
